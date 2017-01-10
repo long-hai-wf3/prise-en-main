687e17f2700363cd0ec1781c8199b3769766a780
oulipo <laudesj@gmail.com>
2017-01-10 11:30:33 +0100

    base de index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>MON SITE AVEC GITHUB</title>
+    </head>
+    <body>
+        <header>
+            <h1>MON SITE AVEC GITHUB</h1>
+        </header>
+    </body>
+</html>
