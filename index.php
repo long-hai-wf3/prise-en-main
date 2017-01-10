cbf0b354ffb23b450bbc1cef59bafae0e39db24b
oulipo <laudesj@gmail.com>
2017-01-10 11:39:09 +0100

    ajout de main et de footer dans index.php

--- index.php
+++ index.php
@@ -8,5 +8,13 @@
         <header>
             <h1>MON SITE AVEC GITHUB</h1>
         </header>
+        <main>
+            <section>
+                <h2>MA PREMIERE SECTION</h2>
+            </section>
+        </main>
+        <footer>
+            <p>tous droits réservés</p>
+        </footer>
     </body>
 </html>
